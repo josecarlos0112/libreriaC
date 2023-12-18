c23c445fc29c4de47829741844dff39b9f0e3b12
josecarlos0112 <jzorrgar@myuax.com>
2023-12-19 00:13:28 +0100

    actualizacion para ej.3

--- AbrirArchivos/main.c
+++ AbrirArchivos/main.c
@@ -9,9 +9,33 @@ int main() {
     if (file == NULL) {
         printf("El archivo %s no se pudo abrir.\n", filename);
         return 1;
+    }else{
+        printf("El archivo %s se abrio correctamente.\n", filename);
     }
 
     // Cerrar el archivo al final
+    fclose(file);
+
+    printf("\nParte b) Leer un archivo\n");
+
+    fprintf(file, "Hola, mundo!\n");
+
+    // Cerrar el archivo después de escribir
+    fclose(file);
+
+    // Abrir de nuevo para leer
+    file = fopen("../hola.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
+
+    char buffer[50];
+    fscanf(file, "%s", buffer);
+
+    printf("Leído del archivo: %s\n", buffer);
+
+    // Cerrar el archivo después de leer
     fclose(file);
 
     return 0;
