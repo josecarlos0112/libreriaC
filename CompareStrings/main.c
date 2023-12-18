c468e25a988e4ec77dc92ea886ab2706a3bf1d6c
josecarlos0112 <jzorrgar@myuax.com>
2023-12-18 23:19:34 +0100

    Actualizacion y pruebas con el main.c

--- CompareStrings/main.c
+++ CompareStrings/main.c
@@ -2,15 +2,26 @@
 #include <stdio.h>
 
 int main() {
-    char str1[] = "Hola estimado";
-    char str2[] = "Ruben";
+    //Prueba 1
+    char str1[] = "Hola Ruben";
+    char str2[] = "Hola Ruben";
+    //Prueba 2
+    char str3[] = "Como estás?";
+    char str4[] = "Yo estoy bien";
 
     int comparison = strcmp(str1, str2);
+    int comparison1 = strcmp(str3, str4);
 
     if (comparison == 0) {
-        printf("Las cadenas son iguales.\n");
+        printf("En la primera prueba\n Las cadenas son iguales.\n");
     } else {
-        printf("Las cadenas son diferentes.\n");
+        printf("En la primera prueba\n Las cadenas son diferentes.\n");
+    }
+
+    if (comparison1 == 0) {
+        printf("En la segunda prueba\n Las cadenas son iguales.\n");
+    } else {
+        printf("En la segunda prueba\n Las cadenas son diferentes.\n");
     }
 
     return 0;
